ad8ca94cbf64c1e0a3766d1f69c456f7015338c1
Lucas Pimentel-Ordyna <lucas.pimentel@datadoghq.com>
2019-07-18 14:57:50 -0400

    update docker image and cmake, use vcpkg

--- docker/Datadog.Trace.ClrProfiler.Native.sh
+++ docker/Datadog.Trace.ClrProfiler.Native.sh
@@ -7,7 +7,10 @@ cd "$DIR/.."
 
 cd src/Datadog.Trace.ClrProfiler.Native
 mkdir -p obj/Debug/x64
-(cd obj/Debug/x64 && cmake ../../.. && make)
+(cd obj/Debug/x64 && \
+ rm -rf *
+ cmake -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake ../../.. && \
+ make)
 
 mkdir -p bin/Debug/x64
 cp -f obj/Debug/x64/Datadog.Trace.ClrProfiler.Native.so bin/Debug/x64/Datadog.Trace.ClrProfiler.Native.so
